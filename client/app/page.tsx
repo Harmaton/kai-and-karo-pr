33e3776c3b2a451deb3953cb24f68609f4143897
Harmaton <njagiiharmaton@gmail.com>
2024-06-28 13:23:10 +0300

    Changed the UI and redirects

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -19,14 +19,24 @@ const Home = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gray-800 flex flex-col items-center justify-center  text-white">
+    <div className="min-h-screen bg-gray-800 flex flex-col items-center justify-center mb-2  text-white">
      <main className="flex flex-col items-center justify-center flex-1 w-full px-4">
         <Header />
+        <div>
+        <h1 className="text-4xl md:text-6xl font-serif font-extrabold text-yellow-300 my-8">
+          Discover Your Perfect Car
+        </h1>
+        <p className="text-lg md:text-xl mb-8">
+          Use Our Advanced <span className="text-yellow-500 font-serif ">AI System</span>  to Self-Recommend Cars Without Being an Expert
+        </p>
+        </div>
         <div className="w-full max-w-4xl">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {items.map((item, index) => (
-              <a href={item.href} className="group items-center hover:bg-gray-500 flex flex-row border p-4 rounded-lg" key={index}>
+              <a href={item.href} className="group items-center shadow-lg hover:bg-gray-500 flex flex-row border p-4 rounded-lg" key={index}>
+                <div className="">
                 <item.icon className={`h-12 w-12 mb-4 mr-2 ${item.color} group-hover:text-white`} />
+                </div>
                 <div className="text-xl font-semibold text-center">{item.text}</div>
               </a>
             ))}
