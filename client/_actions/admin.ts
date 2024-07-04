49eae77a2b600e84c42dc8507bf3f6ab5c8382a5
Harmaton <njagiiharmaton@gmail.com>
2024-07-04 10:08:07 +0300

    redesigned ui

--- client/_actions/admin.ts
+++ client/_actions/admin.ts
@@ -1,29 +1,28 @@
-'use server'
+"use server";
 
 import { prismadb } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
-export async function FetchAdminStatus(){
-    try {
-      
-        const { userId } = auth();
-        if (userId) {
-          const user = await prismadb.guest.findUnique({
-            where: {
-              clerkid: userId,
-            },
-            select: {
-              isAdmin: true,
-            },
-          });
-          if (!user) {
-            return false;
-          }
-          const res = user.isAdmin;
-    
-          return res;
-        }
-      } catch (error) {
-        console.log(error);
+export async function FetchAdminStatus() {
+  try {
+    const { userId } = auth();
+    if (userId) {
+      const user = await prismadb.guest.findUnique({
+        where: {
+          clerkid: userId,
+        },
+        select: {
+          isAdmin: true,
+        },
+      });
+      if (!user) {
+        return false;
       }
+      const res = user.isAdmin;
+
+      return res;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,6 +8,7 @@ import {
 } from "@heroicons/react/20/solid";
 import Header from "./_components/head";
 import Navbar from "@/components/Navbar";
+import Link from "next/link";
 
 const Home = () => {
   const items = [
@@ -49,33 +50,34 @@ const Home = () => {
             </h1>
             <p className="text-lg md:text-xl mb-8">
               Use Our Advanced{" "}
-              <span className="text-yellow-500 text-2xl font-serif ">AI </span>{" "}
+              <span className="text-yellow-300 text-2xl font-serif ">AI </span>{" "}
               System to Self-
-              <span className="text-yellow-500 text-2xl font-serif ">
+              <span className="text-yellow-300 text-2xl font-serif ">
                 Recommend
               </span>{" "}
               Cars Without Being an Expert
             </p>
           </div>
-          <div className="text-2xl mb-2 text-center font-bold">Features</div>
+          <div className="text-3xl font-serif mb-2 text-center font-bold">Features</div>
           <div className="w-full max-w-4xl mb-2 ">
-          <div className="grid grid-cols-1 md:grid-cols-2 justify-center lg:grid-cols-4 gap-6">
-  {items.map((item, index) => (
-    <a
-      href={item.href}
-      className="group items-center shadow-lg hover:bg-gray-500 flex flex-row border p-4 rounded-lg"
-      key={index}
-    >
-      <item.icon
-        className={`h-8 w-8 mr-2 ${item.color} group-hover:text-white`}
-      />
-      <div className="text-xl font-semibold text-center">
-        {item.text}
-      </div>
-    </a>
-  ))}
-</div>
-
+            <div className="grid grid-cols-1 md:grid-cols-2  justify-center lg:grid-cols-4 gap-6">
+              {items.map((item, index) => (
+                <Link
+                  href={item.href}
+                  className="group items-center shadow-lg  dark:bg-dot-white/[0.2] bg-dot-black/[0.8] hover:bg-gray-500 flex flex-row border p-4 rounded-lg"
+                  key={index}
+                >
+                  <div className="rounded-full p-2 mr-2 bg-yellow-300">
+                  <item.icon
+                    className={`h-8 w-8 m-auto items-center text-gray-800 group-hover:text-white`}
+                  />
+                  </div>
+                  <div className="text-xl font-semibold text-center">
+                    {item.text}
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
         </main>
       </div>
